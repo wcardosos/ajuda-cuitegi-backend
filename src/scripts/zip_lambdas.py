a240b654faf4c9841a653bfcb4e14912be715229
Wagner Cardoso <wagnerdev01@gmail.com>
2021-11-02 13:16:13 -0300

    feat(scripts/zip_lambdas) Add 'get_import_strings' and 'verify_custom_import' functions

--- src/scripts/zip_lambdas.py
+++ src/scripts/zip_lambdas.py
@@ -14,3 +14,19 @@ def get_path_file(import_str: str) -> str:
     path_file = path + filename
 
     return path_file
+
+def verify_custom_import(file_line: str) -> bool:
+    FROM_REGEX = r'^from\s'
+    IMPORT_REGEX = r'\simport\s'
+
+    return re.search(FROM_REGEX, file_line) and re.search(IMPORT_REGEX, file_line)
+
+def get_import_strings(filepath: str) -> list:
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+
+        import_strings_lines = list(filter(lambda line: verify_custom_import(line), lines))
+
+        import_strings = list(map(lambda line: line.replace('\n', ''), import_strings_lines))
+
+        return import_strings
--- tests/scripts/TestZipLambdas.py
+++ tests/scripts/TestZipLambdas.py
@@ -1,15 +1,42 @@
 from unittest import TestCase, main
-from src.scripts.zip_lambdas import get_path_file
+from unittest.mock import patch
+from src.scripts.zip_lambdas import get_path_file, get_import_strings, verify_custom_import
+from tests.mocks.stubs.FileStub import FileStub
 
 
 class TestZipLambdas(TestCase):
     def test_get_filename_correctly(self) -> None:
-        IMPORT_STR = 'from src.package.Class import Class'
+        MOCK_IMPORT_STR = 'from src.package.Class import Class'
         EXPECTED_FILE_PATH = 'src/package/Class/Class.py'
 
-        result = get_path_file(IMPORT_STR)
+        result = get_path_file(MOCK_IMPORT_STR)
 
         self.assertEqual(result, EXPECTED_FILE_PATH)
+    
+    def test_verify_custom_import_correctly(self) -> None:
+        MOCK_LINE = 'from src.packages.Class import Class'
+
+        result = verify_custom_import(MOCK_LINE)
+
+        self.assertTrue(result)
+
+    def test_verify_custom_import_returns_false_when_not_line_of_import(self) -> None:
+        MOCK_LINE = 'n = 1'
+
+        result = verify_custom_import(MOCK_LINE)
+
+        self.assertFalse(result)
+    
+    @patch('src.scripts.zip_lambdas.open', return_value=FileStub())
+    def test_get_import_strings(self, file_stub) -> None:
+        EXPECTED_IMPORT_STRINGS = [
+            'from src.packages.Class import Class',
+            'from src.packages.OtherClass import OtherClass'
+        ]
+
+        result = get_import_strings('path')
+
+        self.assertEqual(result, EXPECTED_IMPORT_STRINGS)
 
 if __name__ == '__main__':
     main()
