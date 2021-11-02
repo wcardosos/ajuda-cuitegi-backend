cd86476fd92d2896e64e8e6de7a6f99561fae260
Wagner Cardoso <wagnerdev01@gmail.com>
2021-11-02 13:26:59 -0300

    refactor(scripts/zip_lambdas) Refactoring import string reference on 'get_path_file' function

--- src/scripts/zip_lambdas.py
+++ src/scripts/zip_lambdas.py
@@ -1,12 +1,12 @@
 import re
 
 
-def get_path_file(import_str: str) -> str:
+def get_path_file(import_string: str) -> str:
     PACKAGE_REGEX = r'src.\w+.\w+'
     FILE_REGEX = r'\w+$'
     
-    module: str = re.search(PACKAGE_REGEX, import_str).group(0)
-    object_imported: str = re.search(FILE_REGEX, import_str).group(0)
+    module: str = re.search(PACKAGE_REGEX, import_string).group(0)
+    object_imported: str = re.search(FILE_REGEX, import_string).group(0)
 
     path: str = module.replace('.', '/')
     filename: str = '/' + object_imported + '.py'
